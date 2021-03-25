12879819acc828b0e05674dd3c26935abccbcaff
Pablo Christian <pablo@linearsistemas.com.br>
2021-03-25 12:26:57 -0300

    adição arquivos iniciais

---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { uuid } = require('uuidv4');
+const app = express();
+app.use(express.json());
+
+const projects = [];
+
+// GET 
+app.get('/projects', (request, response) => {
+  const { title } = request.query;
+  const results = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+  return response.json(results);
+});
+
+// POST
+app.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+  return response.json(project);
+});
+
+// PUT
+app.put('/projects/:id', (request, response) => {
+  const { id, title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id == id);
+  if(projectIndex < 0) {
+    return response.status(400).json({ "error": "Project not found." });
+  }
+  const project = {
+    id,
+    title,
+    owner
+  };
+  projects[projectIndex] = project
+  return response.json(project);
+});
+
+// DELETE
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => project.id == id);
+  if(projectIndex < 0) {
+    return response.status(400).json({ "error": "Project not found." });
+  }
+  projects.splice(projectIndex,1);
+  return response.status(204).send();
+});
+
+// LISTEN PORT
+app.listen(3333, () => {
+  console.log('😉️ Back-end iniciado!');
+});
